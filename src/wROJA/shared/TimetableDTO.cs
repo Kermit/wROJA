6fe17143d00910499832e42c520e0ce469780bac
Maciej Płaza <maciejplaza@fastmail.fm>
2013-05-16 21:07:38 +0200

    DTO dla tabeli rozkładów - może pochodzić z różnych webserwisów.
    Signed-off-by: Maciej Płaza <maciejplaza@fastmail.fm>

---
+++ src/wROJA/shared/TimetableDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wROJA.shared
+{
+    public class TimetableDTO
+    {
+        public string DayName { get; set; }
+        public string Table { get; set; }
+        public string Legend { get; set; }
+
+        public TimetableDTO() { }
+
+        public TimetableDTO(string dayName, string table, string legend)
+        {
+            DayName = dayName;
+            Table = table;
+            Legend = legend;
+        }
+    }    
+}
