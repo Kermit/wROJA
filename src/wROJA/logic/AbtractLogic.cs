3cb9cfcc180ce4ad5608767f86885cfd5c7523ba
Maciej Płaza <maciejplaza@fastmail.fm>
2013-05-20 17:29:46 +0200

    Poprawki wyglądu rozkładu jazdy. Poszerzenie okna i kolumn.
    Signed-off-by: Maciej Płaza <maciejplaza@fastmail.fm>

--- src/wROJA/logic/AbtractLogic.cs
+++ src/wROJA/logic/AbtractLogic.cs
@@ -14,22 +14,41 @@ namespace wROJA.logic
         protected List<Paragraph> PrepareTimetable(List<TimetableDTO> timetable)
         {
             List<Paragraph> paragraphs = new List<Paragraph>();
+            string legends = string.Empty;
 
             foreach (TimetableDTO obj in timetable)
             {
-                Run dayNameRun = new Run("<sup>" + obj.DayName + "</sup>");
-                dayNameRun.FontSize = 12;
-                dayNameRun.FontWeight = FontWeights.Bold;
+                Run dayNameRun = new Run(obj.DayName);                
+                dayNameRun.FontWeight = FontWeights.Bold;                
 
-                //Dodać ładneijszy wygląd
                 Paragraph p = new Paragraph();
+                p.FontFamily = new System.Windows.Media.FontFamily("Palatino Linotype");
+                p.FontSize = 12;
                 p.Inlines.Add(dayNameRun);
                 p.Inlines.Add(Environment.NewLine);
-                p.Inlines.Add(new Run(obj.Table));
-                p.Inlines.Add(Environment.NewLine);
-                p.Inlines.Add(new Run(obj.Legend));
+
+                foreach (String hour in obj.Table.Split(','))
+                {
+                    string[] splittedHour = hour.Split(':');                    
+
+                    Run hourRun = new Run(splittedHour[0]);
+                    Run minutesRun = new Run(splittedHour[1].Replace(';', ' '));
+                    minutesRun.Typography.Variants = FontVariants.Superscript;
+                    
+                    p.Inlines.Add(hourRun);
+                    p.Inlines.Add(minutesRun);
+                    p.Inlines.Add(" ");
+                }
+
+                legends += obj.Legend;           
                 paragraphs.Add(p);
             }
+
+            Paragraph legendParagraph = new Paragraph();
+            legendParagraph.FontFamily = new System.Windows.Media.FontFamily("Palatino Linotype");
+            legendParagraph.FontSize = 12;
+            legendParagraph.Inlines.Add(new Run(legends.Replace(";", Environment.NewLine)));
+            paragraphs.Add(legendParagraph);
 
             return paragraphs;
         }
